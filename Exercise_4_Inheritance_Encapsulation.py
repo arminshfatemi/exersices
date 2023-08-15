0fe3cb748f3e4e3c4888aa716f1d6b19863c020a
Armin sh fatemi <arminsh610@gmail.com>
2023-08-16 01:12:08 +0330

    24/6/1403 exercise update

--- Exercise_4_Inheritance_Encapsulation.py
+++ Exercise_4_Inheritance_Encapsulation.py
@@ -1,29 +1,29 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, __name, __age):
+        self._name = __name
+        self._age = __age
 
 
 class Student(Person):
-    def __init__(self, name: str, age: int, student_id: int):
-        self.student_id = student_id
-        Person.__init__(self, name, age)
+    def __init__(self, __name: str, __age: int, __student_id: int):
+        self.__student_id = __student_id
+        Person.__init__(self, __name, __age)
 
     def get_all_info(self):
         print(f"""
-        student name: {self.name}
-        student age:{self.age}
-        student id: {self.student_id}
+        student name: {self._name}
+        student age:{self._age}
+        student id: {self.__student_id}
         """)
 
-    def name_age_change(self, name: str, age: int, student_id: int):
-        self.name = name
-        self.age = age
-        self.student_id = student_id
+    def name_age_change(self, name: str, age: int, __student_id: int):
+        self._name = name
+        self._age = age
+        self.__student_id = __student_id
         print(f"""
-        student name: {self.name}
-        student age:{self.age}
-        student id: {self.student_id}
+        student name: {self._name}
+        student age:{self._age}
+        student id: {self.__student_id}
         """)
 
 
